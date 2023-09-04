02e213701d673d8d1ae18c86bebdf0d3a2611bf9
cyber06917 <lalitkumarcyber069@gmail.com>
2023-09-04 19:32:52 +0530

    Add Firebase signup authentication feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -37,10 +41,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/cyberchat/VerificationActivity.kt
+++ app/src/main/java/com/example/cyberchat/VerificationActivity.kt
@@ -2,35 +2,86 @@ package com.example.cyberchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class VerificationActivity : AppCompatActivity() {
 
-    private lateinit var loginUserName: EditText
-    private lateinit var loginPassword: EditText
+    private lateinit var myAuth:FirebaseAuth
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
-    private lateinit var outputResult: TextView
+    private lateinit var signUpTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification)
         initializeViews()
-        setupLoginButton()
+
+
+        signUpTextView.setOnClickListener {
+            isEmailValid()
+        }
     }
 
     private fun initializeViews() {
-        loginUserName = findViewById(R.id.usernameEditText)
-        loginPassword = findViewById(R.id.passwordEditText)
-        loginButton = findViewById(R.id.loginButton)
-        outputResult = findViewById(R.id.output)
+        emailEditText = findViewById(R.id.emailEditTextId)
+        passwordEditText = findViewById(R.id.passwordEditTextId)
+        loginButton = findViewById(R.id.loginButtonId)
+        signUpTextView = findViewById(R.id.signupLinkId)
+        signUpTextView.isClickable = true
+        myAuth = FirebaseAuth.getInstance()
+
     }
 
-    private fun setupLoginButton() {
-        loginButton.setOnClickListener {
-            val welcomeMessage = "Welcome ${loginUserName.text}"
-            outputResult.text = welcomeMessage
+    private fun signUp(emailPhone:String, password:String) {
+
+        if (emailPhone.isNotEmpty() && password.isNotEmpty()) {
+            myAuth.createUserWithEmailAndPassword(emailPhone, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Registration successful
+                        Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // Registration failed
+                        Toast.makeText(this, "Registration failed: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        } else {
+
+            Toast.makeText(this, "Password or Email/Phone is empty", Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+    // Validate a email
+    private fun isEmailValid(){
+        // signup email & password
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+        val emailPattern = "[a-zA-Z0-9._%+-]+@(gmail\\.com|outlook\\.com)"
+        val valid = email.matches(emailPattern.toRegex())
+
+        // Email validator
+        if (valid) {
+            signUp(email, password)
+        } else {
+            Toast.makeText(this, "email is not valid", Toast.LENGTH_SHORT).show()
         }
     }
+
+    // Validate a password
+    private fun isPasswordValid(password: String): Boolean {
+        val passwordPattern = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"
+        return password.matches(passwordPattern.toRegex())
+    }
+
+
+
 }
